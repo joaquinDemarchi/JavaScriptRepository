318331047e19a08df1c360d201728b727227e121
JoaDem <joa.demarchi@gmail.com>
2022-11-12 17:35:03 -0300

    Trabajando en estilos de ventada de compra

--- CURSOS/JSPractico2022_Platzi/Repo_CursoFrontend_Platzi/main.js
+++ CURSOS/JSPractico2022_Platzi/Repo_CursoFrontend_Platzi/main.js
@@ -5,7 +5,7 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 
@@ -15,20 +15,20 @@ menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 
 function toggleDesktopMenu() {
-  const isAsideClosed = aside.classList.contains('inactive');
+  const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
   if (!isAsideClosed) {
-    aside.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
   }
 
   desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
-  const isAsideMenuClosed = aside.classList.contains('inactive');
+  const isAsideMenuClosed = shoppingCartContainer.classList.contains('inactive');
 
   if (!isAsideMenuClosed) {
-    aside.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
   }
   mobileMenu.classList.toggle('inactive');
 }
@@ -43,7 +43,7 @@ function toggleCarritoAside(){
     desktopMenu.classList.add('inactive');
   }
 
-  aside.classList.toggle('inactive')
+  shoppingCartContainer.classList.toggle('inactive')
 
 }
 // incluimos el listado de articulos desde JS
@@ -126,6 +126,7 @@ function renderProducts(arr){
   }
 }
 
+renderProducts(productList);
 
 
 // const productDetailCloseIcon = document.querySelector('.product-detail-close')
