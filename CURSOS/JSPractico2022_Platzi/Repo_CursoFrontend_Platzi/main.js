7ea833b9f5e1c63f6311b60ffe37da25f0f578ac
JoaDem <joa.demarchi@gmail.com>
2022-11-12 16:22:47 -0300

    Avance hasta la clase 21

--- CURSOS/JSPractico2022_Platzi/Repo_CursoFrontend_Platzi/main.js
+++ CURSOS/JSPractico2022_Platzi/Repo_CursoFrontend_Platzi/main.js
@@ -6,6 +6,7 @@ const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -45,6 +46,86 @@ function toggleCarritoAside(){
   aside.classList.toggle('inactive')
 
 }
+// incluimos el listado de articulos desde JS
+//primero convertimos en objetos la info de los art y almacenamso en un array
+const productList = [];
+productList.push({
+  name: 'bike',
+  price: 120,
+  image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+  name: 'pantalla',
+  price: 200,
+  image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+productList.push({
+  name: 'compu',
+  price: 320,
+  image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+//..luego recorremos el array utilizando For of
+
+/* <div class="product-card">
+        <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+        <div class="product-info">
+          <div>
+            <p>$120,00</p>
+            <p>Bike</p>
+          </div>
+          <figure>
+            <img src="./icons/bt_add_to_cart.svg" alt="">
+          </figure>
+        </div>
+      </div> */
+
+
+// es importante encapsular nuestro for en una funcion asi le podemos dar funcionalidades extra
+function renderProducts(arr){
+  for (product of arr){
+    // console.log(product.name); esto nos devolveria todos los nombres de los articulos 
+    //ahora por c/ producto ...
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card')
+
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src',product.image);
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    const productInfoDiv = document.createElement('div');
+    
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+
+    const productName = document.createElement('p');
+    productName.innerText =  product.name;
+
+    //usamos append child para insertar etiquetas unas dentro de otras
+
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+    
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg');
+
+    productInfoFigure.appendChild(productImgCart);
+
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+
+    cardsContainer.appendChild(productCard);
+  }
+}
+
 
 
 // const productDetailCloseIcon = document.querySelector('.product-detail-close')
