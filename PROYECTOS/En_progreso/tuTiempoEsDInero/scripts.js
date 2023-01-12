ef3fc1181b62993d23a33a213e594787b3337148
JoaDem <joa.demarchi@gmail.com>
2023-01-12 07:03:44 -0300

    mejoras a realizar

--- PROYECTOS/En_progreso/tuTiempoEsDInero/scripts.js
+++ PROYECTOS/En_progreso/tuTiempoEsDInero/scripts.js
@@ -12,6 +12,8 @@ botonCalcular.addEventListener("click", convertir);
 
 function convertir(){
 
+   //declaracion de variables poner fuera de la funcion y probar si funciona dandole parametros que necesite.
+
    const gas = gasto.value;
    const hs = hsTrabMes.value;
    const sal = salMes.value;
@@ -22,13 +24,19 @@ function convertir(){
    var gastoEnMin = Math.ceil(gas / salXmin);
    var minRestantes = Number(gastoEnMin % 60);
 
-   console.log({gastoEnMin, minRestantes})
+   // console.log({gastoEnMin, minRestantes})
 
    if (gastoEnMin < 60){
       if (gastoEnMin == 1){
-         return resultadoEnP.innerText = gastoEnMin + ' minuto.';
+         //crear otra variable que englobe todo el output
+         // var outputFinal = gastoEnMin + ' minuto.'
+         //o 
+         // var outputFinal = gastoEnMin + ' horas y ' + minRestantes + ' minuto.'
+         //dejando un solo return asi..
+         //return resultadoEnP.innerText = outputFinal.
+         return resultadoEnP.innerText = gastoEnMin + ' minuto';
       } else {
-         return resultadoEnP.innerText = gastoEnMin + ' minutos.';
+         return resultadoEnP.innerText = gastoEnMin + ' minutos';
       }
    } else {
       if (minRestantes == 0){
@@ -52,15 +60,15 @@ function convertir(){
             }
          } else {
             if (minRestantes == 1){
-               return resultadoEnP.innerText = gastoEnMin + ' horas y ' + minRestantes + ' minuto.';
+               return resultadoEnP.innerText = gastoEnMin + ' horas y ' + minRestantes + ' minuto';
             } else { 
-               return resultadoEnP.innerText = gastoEnMin + ' horas y ' + minRestantes + ' minutos.';
+               return resultadoEnP.innerText = gastoEnMin + ' horas y ' + minRestantes + ' minutos';
             }
          }
       }
    }
 
-   
+   //crear otra funcion que le agregue algo al output pelado. Como "Perdiste xxxx de tu vida con ese gasto."
 }
 
 
